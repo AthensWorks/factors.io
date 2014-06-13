ba411fdc8fc586b0d7f7d6451ebb948c13bd8b31
Ricky Chilcott <ricky@rbsavvy.com>
2014-06-13 18:57:08 -0400

    Generating route scaffold

--- factors_app.rb
+++ factors_app.rb
@@ -5,18 +5,24 @@ class FactorsApp < Sinatra::Base
     "Hello Factorized World"
   end
 
+  get '/about' do
+    "Who built this? And why?"
+  end
+
   # Get a random number
   get '/random' do
+    "TODO: GET /random"
   end
 
   # Show a number
   get '/factors/:number' do
+    "TODO: GET /factors/#{params[:number]}"
   end
 
   # Submit a number
   post '/factors/:number' do
+    "TODO: POST /factors/#{params[:number]}"
   end
-
 
   # start the server if ruby file executed directly
   run! if app_file == $0
