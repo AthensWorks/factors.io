9d652fbed672b63b7351b9edf3e90e3231cb36b8
Ricky Chilcott <ricky@rbsavvy.com>
2014-07-25 12:25:42 -0400

    Don’t need to set this

--- lib/workers/factor_and_divisor_worker.rb
+++ lib/workers/factor_and_divisor_worker.rb
@@ -1,15 +1,5 @@
 require 'sidekiq'
 
-if Rails.env.production?
-  Sidekiq.configure_server do |config|
-    config.redis = { url: ENV["REDISTOGO_URL"]}
-  end
-
-  Sidekiq.configure_client do |config|
-   config.redis = { url: ENV["REDISTOGO_URL"]}
-  end
-end
-
 class FactorAndDivisorWorker
   include Sidekiq::Worker
 
